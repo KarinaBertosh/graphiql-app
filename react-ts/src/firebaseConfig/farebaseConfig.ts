86f7239f4d2118f1dc62242a66ee6d43ec050e60
elina <elina.nep@gmail.com>
2023-05-03 23:57:57 +0300

    feat: add farebase config

---
+++ react-ts/src/firebaseConfig/farebaseConfig.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyAWYCXiakkL_y4F0FrzPtikuYghOau1zxk",
+  authDomain: "graphiql-d50d8.firebaseapp.com",
+  projectId: "graphiql-d50d8",
+  storageBucket: "graphiql-d50d8.appspot.com",
+  messagingSenderId: "915932982147",
+  appId: "1:915932982147:web:77bbb6ca4a102b18d264b6",
+};
