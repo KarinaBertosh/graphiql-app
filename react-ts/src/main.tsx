985be6d063b22e6bb56f00c7fbb093ec0b2791c8
elina <elina.nep@gmail.com>
2023-05-10 19:51:04 +0300

    feat: basic structure

--- react-ts/src/main.tsx
+++ react-ts/src/main.tsx
@@ -1,14 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import firebase from "firebase/compat/app";
 import { firebaseConfig } from "./firebaseConfig/farebaseConfig";
-import App from "./App.tsx";
-import "./index.css";
+import "./main.scss";
+
+import { routerObj } from "./router/RouterConfig";
 
 firebase.initializeApp(firebaseConfig);
+const router = createBrowserRouter(routerObj);
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
