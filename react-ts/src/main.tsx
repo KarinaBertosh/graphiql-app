5f30a9655a42163eb08591bf27e74803d5b25f27
KarinaBertosh <cherrycake246@gmail.com>
2023-05-15 17:16:40 +0300

    feat: add get data method for graph ql

--- react-ts/src/main.tsx
+++ react-ts/src/main.tsx
@@ -6,14 +6,18 @@ import { firebaseConfig } from "./firebaseConfig/farebaseConfig";
 import "./localization/i18next";
 import "./main.scss";
 import { routerObj } from "./router/RouterConfig";
+import { ApolloProvider } from "@apollo/client";
+import  client  from "./apollo/client";
 
 firebase.initializeApp(firebaseConfig);
 const router = createBrowserRouter(routerObj);
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <Suspense fallback="...loading">
-      <RouterProvider router={router} />
-    </Suspense>
+    <ApolloProvider client={client}>
+      <Suspense fallback="...loading">
+        <RouterProvider router={router} />
+      </Suspense>
+    </ApolloProvider>
   </React.StrictMode>
 );
--- react-ts/src/pages/MainPage.tsx
+++ react-ts/src/pages/MainPage.tsx
@@ -1,11 +1,44 @@
 import { useState } from "react";
 import doc from "../assets/doc.png";
+import { useQuery, gql } from "@apollo/client";
 import start from "../assets/start.png";
 
 export const MainPage = () => {
+  const DEFAULT_DATA = gql`
+    query MyQuery {
+      character(id: "2") {
+        origin {
+          id
+        }
+        location {
+          id
+        }
+        episode {
+          id
+        }
+      }
+    }
+  `;
+  const [addData, setAddData] = useState(DEFAULT_DATA);
   const [isOpenDocumentation, setIsOpenDocumentation] = useState(false);
   const openDocumentation = () => {
     setIsOpenDocumentation(!isOpenDocumentation);
+  };
+
+  const GET_DATA = gql`
+    ${addData}
+  `;
+
+  function getDate() {
+    const { loading, error, data } = useQuery(GET_DATA);
+    if (loading) return <p>Loading...</p>;
+    if (error) return <p>Error </p>;
+        return JSON.stringify(data);
+  }
+
+  const sendData = () => {
+    const value = document.getElementById("addData")?.value;
+    setAddData(value);
   };
 
   return (
@@ -15,11 +48,15 @@ export const MainPage = () => {
         {isOpenDocumentation ? <div>Documentation</div> : ""}
       </div>
       <div className="redactor">
-        <textarea placeholder="Code"></textarea>
-        <textarea placeholder="Variables"></textarea>
-        <img src={start} className="documentation__button__start" />
+        <input id="addData" placeholder="Code"></input>
+        <input placeholder="Variables"></input>
+        <img
+          src={start}
+          className="documentation__button__start"
+          onClick={sendData}
+        />
       </div>
-      <textarea placeholder="received code"></textarea>
+      <input placeholder="received code" value={getDate()}></input>
     </div>
   );
 };
