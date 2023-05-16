79106b114cc1026fc9e04cc6528e349f928d6b3c
KarinaBertosh <cherrycake246@gmail.com>
2023-05-16 15:52:12 +0300

    feat: add errors processing

--- react-ts/src/pages/MainPage.tsx
+++ react-ts/src/pages/MainPage.tsx
@@ -4,21 +4,23 @@ import { useQuery, gql } from "@apollo/client";
 import start from "../assets/start.png";
 
 export const MainPage = () => {
-  const DEFAULT_DATA = gql`
-    query MyQuery {
-      character(id: "2") {
-        origin {
-          id
-        }
-        location {
-          id
-        }
-        episode {
-          id
-        }
+  const defaultValue = `query MyQuery {
+    character(id: "2") {
+      origin {
+        id
+      }
+      location {
+        id
+      }
+      episode {
+        id
       }
     }
+  }`;
+  const DEFAULT_DATA = gql`
+    ${defaultValue}
   `;
+
   const [addData, setAddData] = useState(DEFAULT_DATA);
   const [isOpenDocumentation, setIsOpenDocumentation] = useState(false);
   const openDocumentation = () => {
@@ -30,14 +32,19 @@ export const MainPage = () => {
   `;
 
   function getDate() {
-    const { loading, error, data } = useQuery(GET_DATA);
-    if (loading) return <p>Loading...</p>;
-    if (error) return <p>Error </p>;
-        return JSON.stringify(data);
+    try {
+      const { loading, error, data } = useQuery(GET_DATA);
+      if (loading) return `Loading ...`;
+      if (error) return `Error: ${JSON.stringify(error.message)}`;
+      return data;
+    } catch (errors) {
+      console.error(errors);
+    }
   }
 
   const sendData = () => {
-    const value = document.getElementById("addData")?.value;
+    const value = document.getElementById("addData")!.value;
+    console.log("value", value);
     setAddData(value);
   };
 
@@ -48,15 +55,22 @@ export const MainPage = () => {
         {isOpenDocumentation ? <div>Documentation</div> : ""}
       </div>
       <div className="redactor">
-        <input id="addData" placeholder="Code"></input>
-        <input placeholder="Variables"></input>
+        <textarea
+          id="addData"
+          placeholder="Code"
+          defaultValue={defaultValue}
+        ></textarea>
+        <textarea placeholder="Variables"></textarea>
         <img
           src={start}
           className="documentation__button__start"
           onClick={sendData}
         />
       </div>
-      <input placeholder="received code" value={getDate()}></input>
+      <textarea
+        placeholder="received code"
+        value={JSON.stringify(getDate())}
+      ></textarea>
     </div>
   );
 };
