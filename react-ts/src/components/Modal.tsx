68491b86c8af8bc49dfdc2345ea76457cc3a9970
KarinaBertosh <cherrycake246@gmail.com>
2023-05-18 16:59:45 +0300

    fix: modal

--- react-ts/src/components/Modal.tsx
+++ react-ts/src/components/Modal.tsx
@@ -1,21 +1,27 @@
-export const Modal = (
-  error: any,
-  active: boolean,
-  setActive: () => void
-) => {
+export const Modal = ({
+  error,
+  active,
+  setActive,
+}: {
+  error: string;
+  active: boolean;
+  setActive: () => void;
+}) => {
   console.log("active", active);
 
   return (
     <div
       id="myModal"
       className={active ? "modal" : "modal none"}
-      onClick={(e) => e.currentTarget === e.target && setActive()}
+      onClick={(e) => {
+        e.currentTarget === e.target && setActive();
+      }}
     >
       <div className="modal-content">
-        <div className="close" onClick={setActive}>
+        <div className="close" onClick={() => setActive()}>
           &times;
         </div>
-        <p>{error.error}</p>
+        <p>{error}</p>
       </div>
     </div>
   );
