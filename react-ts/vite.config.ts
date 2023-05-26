7d7e1901625300cc916bc18a20fbfcfdc5d999fe
elina <elina.nep@gmail.com>
2023-05-26 15:30:28 +0300

    add: yml

--- react-ts/vite.config.ts
+++ react-ts/vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // base: "/graphiql-app",
+  base: "/graphiql-app",
   plugins: [react()],
 
   build: {
