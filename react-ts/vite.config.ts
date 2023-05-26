cbe225c55bb16d8e99fb88c81dcfefd3d3694a58
KarinaBertosh <cherrycake246@gmail.com>
2023-05-26 11:30:27 +0300

    fix: deploy

--- react-ts/vite.config.ts
+++ react-ts/vite.config.ts
@@ -1,8 +1,42 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: "/graphiql-app", // закоменчивать эту строчку если в режиме разработки. включать эту строчку при деплое
   plugins: [react()],
-  base: "/graphiql-app"
+  json: {
+    namedExports: true,
+    stringify: true,
+  },
 });
+
+// export default defineConfig(({ command, mode }) => {
+//   if (command === "build") {
+//     return {
+//       plugins: [react()],
+//       base: "/src",
+//       publicDir: "/src",
+//       json: {
+//         namedExports: true,
+//         stringify: true
+//       },
+//       esbuild: {
+//         jsxFactory: 'h',
+//         jsxFragment: 'Fragment',
+//       },
+//       server: {
+//         cors: false
+//       },
+//       build: {
+//         outDir: "/dist/",
+//         // assetsDir: "/assets",
+//         // cssCodeSplit: false,
+//         manifest: true
+//       }
+//     };
+//   } else {
+//     return {
+//       plugins: [react()],
+//     };
+//   }
+// });
